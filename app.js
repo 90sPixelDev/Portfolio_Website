a3b4dddba3a92c6ebde33a746cbabe9b0c4fd9bb
90sPixelDev <fontlien@gmail.com>
2022-04-22 01:42:55 -0400

    Added function that hides mobile men u when pressing anywhere

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const navParent = document.getElementById('nav-items-parent');
 const menuBtn = document.getElementById('nav-toggle');
+const navToggle =  document.getElementById('nav-toggle');
 
+// Opening and closing menu by adding or removing class
 menuBtn.addEventListener('click', ()=> {
     if (navParent.classList.contains('hide-menu')) {
         navParent.classList.add('show-menu');
@@ -11,3 +13,13 @@ menuBtn.addEventListener('click', ()=> {
         navParent.classList.remove('show-menu');
     }
 })
+
+
+// This is to check if anything else is pressed while menu is open to close it
+document.addEventListener('click', (e) => {
+    const isClickInsideElement = navToggle.contains(e.target);
+    if (!isClickInsideElement) {
+        navParent.classList.add('hide-menu');
+        navParent.classList.remove('show-menu');
+    }
+});
