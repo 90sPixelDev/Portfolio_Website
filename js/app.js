fdd09eb293b19200d31fe906b782ac8962e982d9
90sPixelDev <fontlien@gmail.com>
2022-04-30 03:00:56 -0400

    added fade in affect on scrolling learned how to calculate the client window height Did a bit of refactoring added a top-top button (not sure about the style will probably change it later on)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const navParent = document.getElementById('nav-items-parent');
 const menuBtn = document.getElementById('nav-toggle');
 const navToggle =  document.getElementById('nav-toggle');
+const myName = document.querySelector('#my-name');
+const scrollEl = document.querySelectorAll('.scroll-anim');
 
 // Opening and closing menu by adding or removing class
 menuBtn.addEventListener('click', ()=> {
@@ -21,4 +23,38 @@ document.addEventListener('click', (e) => {
         navParent.classList.add('hide-menu');
         navParent.classList.remove('show-menu');
     }
-});
+})
+
+// Function to check if a HTML element has been srolled a certain percentage by checking the client's height size and scrolling
+// Passing in the html element, and the amount to scroll
+const elementInView = (el, scrollOffset) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (elementTop <= ((Window.innerHeight || document.documentElement.clientHeight) - scrollOffset))
+}
+
+// Making frunction to assign a class that will make the element fade in
+const displayScrolledElement = (el) => {
+    el.classList.add('scrolled');
+}
+
+const scrollAnimManager = () => {
+    scrollEl.forEach(el => {
+        if (elementInView(el, 100)) {
+            displayScrolledElement(el);
+        }
+        else {
+            el.classList.remove('scrolled');
+        }
+    });
+    // if (elementInView(scrollEl, 100)) {
+    //     displayScrolledElement(scrollEl);
+    // }
+    // else {
+    //     scrollEl.classList.remove('scrolled');
+    // }
+}
+
+window.addEventListener('scroll', () => {
+    scrollAnimManager();
+})
